9859b2d22715583dd06bc48e2be1e3db4b584953
Vengadesvaran <32575089+Vengadesvaran@users.noreply.github.com>
2023-01-04 04:14:53 +0000

    SumOfAllDigits

---
+++ src/main/java/SumOfAllDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SumOfAllDigits {
+    public static void main(String[] args) {
+        System.out.println("Enter the number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        Integer total = Stream.of(String.valueOf(number).split(""))
+        .collect(Collectors.summingInt(Integer::parseInt)).intValue();
+        System.out.print(total);
+        scanner.close();
+    }
+}
