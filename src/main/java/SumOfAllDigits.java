1ec8f2847208dcdb7e7302357c24b92b73cfe504
Vengadesvaran <vengadesvaran@gmail.com>
2023-01-13 10:21:18 +0530

    Optimized

--- src/main/java/SumOfAllDigits.java
+++ src/main/java/SumOfAllDigits.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class SumOfAllDigits {
-    public static void main(String[] args) {
-        System.out.println("Enter the number: ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        Integer total = Stream.of(String.valueOf(number).split(""))
-                .collect(Collectors.summingInt(Integer::parseInt)).intValue();
-        System.out.print(total);
-        scanner.close();
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int testCases = Integer.parseInt(bufferedReader.readLine());
+        final String[] inputs = bufferedReader.readLine().split(" ");
+        IntStream.range(0, testCases).forEach(i ->
+                System.out.println(Stream.of(inputs[i].split(""))
+                            .mapToInt(Integer::parseInt).sum())
+        );
     }
 }
